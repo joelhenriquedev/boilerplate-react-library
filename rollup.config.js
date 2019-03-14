e1685441551d5d1d02907817c3713b99ed56a646
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2019-03-13 21:49:47 -0300

    Configuration output rollup

--- rollup.config.js
+++ rollup.config.js
@@ -5,12 +5,19 @@ import url from "rollup-plugin-url"
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 
+import pkg from './package.json'
+
 export default {
 	input: 'src/main.js',
-	output: {
-		file: 'bundle.js',
-		format: 'cjs'
-	},
+	output: [{
+		file: pkg.main,
+		format: 'cjs',
+		sourcemap: true
+	},{
+		file: pkg.module,
+		format: 'es',
+		sourcemap: true
+	}],
 	plugins: [
 		external(),
 		commonjs(),
