5fa246799bf8a27c344f92f7c449cadb11aa0435
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2019-03-13 21:09:49 -0300

    installed rollup-plugin-peer-deps-external plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,12 @@
+import external from 'rollup-plugin-peer-deps-external'
+
 export default {
 	input: 'src/main.js',
 	output: {
 		file: 'bundle.js',
 		format: 'cjs'
-	}
+	},
+	plugins: [
+		external()
+	]
 };
